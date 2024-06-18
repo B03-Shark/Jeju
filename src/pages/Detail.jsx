79e33c5c0155d773298392fc325ceff24e1fb38d
heesu21 <gmltncpfl1@naver.com>
2024-06-18 16:14:21 +0900

    modified:   src/pages/Detail.jsx modified:   src/shared/Router.jsx

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,17 @@
 function Detail() {
-  return <div>Detail</div>;
+  const [modalDisplay, setModalDisplay] = useState(false);
+  const handleModal = () => {
+    setModalDisplay((prev) => !prev);
+  };
+
+  console.log(import.meta.env.VITE_SUPABASE_URL);
+  return (
+    <div>
+      {modalDisplay ? <StModalWrapper onClick={handleModal} /> : ''}
+      {modalDisplay ? <Modal setModalDisplay={setModalDisplay} /> : ''}
+      <button onClick={handleModal}>모달 버튼</button>
+    </div>
+  );
 }
 
 export default Detail;
