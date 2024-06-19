6283e740590b0e0cad8fae5968785cce205084a7
yeseul0809 <dresulsul@naver.com>
2024-06-19 22:22:24 +0900

    add : header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,5 +1,58 @@
+import { useNavigate } from 'react-router-dom';
+import search from '../../assets/search.png';
+import store from '../../assets/store.png';
+import styled from 'styled-components';
+
 function Header() {
-  return <div>Header</div>;
+  const navigate = useNavigate();
+
+  return (
+    <StHeader>
+      <StLeftWrap>
+        <StLogoImg
+          onClick={() => {
+            navigate(`/`);
+          }}
+          src={store}
+          alt="logoImg"
+        />
+      </StLeftWrap>
+      <StRightWrap>
+        <StSearchImg src={search} alt="searchImg" />
+      </StRightWrap>
+    </StHeader>
+  );
 }
 
 export default Header;
+
+const StHeader = styled.div`
+  height: 6rem;
+  background-color: #ffc1634a;
+  justify-content: space-between;
+  display: flex;
+  align-items: center;
+  padding: 0 64px;
+  margin-bottom: 20px;
+`;
+const StLeftWrap = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+const StRightWrap = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+`;
+
+const StLogoImg = styled.img`
+  width: 64px;
+  height: 64px;
+  cursor: pointer;
+`;
+
+const StSearchImg = styled.img`
+  width: 43px;
+  height: 43px;
+  border-radius: 50%;
+`;
