ce0e40915ce83ac36cce3138e34f5f41950431d1
YCDM03 <ycdm03@gmail.com>
2024-06-19 09:13:05 +0900

    update: 상세페이지 리뷰카드 추가

---
+++ src/components/Detail/ReviewCard.jsx
@@ -0,0 +1,118 @@
+import styled from 'styled-components';
+
+function ReviewCard({ data }) {
+  const { created_at, content, image_url, nickname } = data;
+  return (
+    <>
+      <StCardDiv>
+        <StUserBox>
+          <StUserAvatar />
+          <div>
+            <StUserName>{nickname}</StUserName>
+            <StUserDate>{created_at}</StUserDate>
+          </div>
+        </StUserBox>
+        <StImgDiv>
+          <img src={image_url} alt="" />{' '}
+        </StImgDiv>
+        <StTextDiv>
+          <StTextP>{content}</StTextP>
+          {/* <StFavorDiv>
+            <StFavorSpan>{item.like}</StFavorSpan>
+          </StFavorDiv> */}
+        </StTextDiv>
+      </StCardDiv>
+    </>
+  );
+}
+
+const StCardDiv = styled.div`
+  width: 260px;
+  height: 330px;
+  border: 2px solid black;
+  border-radius: 20px;
+`;
+
+const StUserBox = styled.div`
+  width: 90%;
+  height: 20px;
+  margin: 10px auto;
+  display: flex;
+  gap: 10px;
+`;
+
+const StUserAvatar = styled.div`
+  width: 30px;
+  height: 30px;
+  border: 2px solid black;
+  border-radius: 17px;
+  background-color: gray;
+`;
+
+const StUserName = styled.h2`
+  width: 100%;
+  margin: 0;
+  text-align: start;
+  font-size: 13px;
+  font-weight: 700;
+`;
+
+const StUserDate = styled.h3`
+  width: 100%;
+  margin: 0;
+  text-align: end;
+  font-size: 10px;
+  font-weight: 700;
+`;
+
+const StH2 = styled.div`
+  box-sizing: border-box;
+  padding: 0 20px;
+  text-align: center;
+  margin-top: 5px;
+  font-weight: 600;
+  width: 100%;
+  height: 1rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+const StNickNameDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 12px 30px 6px 20px;
+`;
+
+const StTextDiv = styled.div`
+  font-size: 1.2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 20px;
+`;
+
+const StTextP = styled.p`
+  font-size: 15px;
+  max-width: 180px;
+  height: 17px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+const StImgDiv = styled.div`
+  margin: 10px auto;
+  align-content: center;
+  img {
+    display: block;
+    margin: 20px auto;
+    width: 100%;
+    max-width: 230px;
+    height: 150px;
+    object-fit: scale-down;
+    border: 2px solid black;
+  }
+`;
+
+export default ReviewCard;
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,18 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Modal from '../components/Detail/Modal';
+import ReviewCard from '../components/Detail/ReviewCard';
+import supabase from '../supabase/supabase';
+import { useParams } from 'react-router-dom';
+
+//임시데이터
+let { data: user, error: userError } = await supabase.from('users').select('*');
 
 function Detail() {
   const [modalDisplay, setModalDisplay] = useState(false);
+  const [review, setReview] = useState([]);
+  const { id: dataCd } = useParams();
+
+  useEffect(() => {
+    const loadData = async () => {
+      let { data: reviewData, error: reviewError } = await supabase.from('reviews').select().eq('dataCd', dataCd);
+      setReview(reviewData);
+    };
+
+    loadData();
+  }, []);
+
   const handleModal = () => {
     setModalDisplay((prev) => !prev);
   };
-
-
   return (
     <div>
       {modalDisplay ? <StModalWrapper onClick={handleModal} /> : ''}
       {modalDisplay ? <Modal setModalDisplay={setModalDisplay} /> : ''}
+      {review.map((e) => {
+        console.log(e);
+        return <ReviewCard key={e.id} data={e} />;
+      })}
+
       <button onClick={handleModal}>모달 버튼</button>
     </div>
   );
